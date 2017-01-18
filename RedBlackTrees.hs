e6ea5fba60732d1adfcc2dcad88198da2e6dbaa2
mallow66 <brahimserghini2@gmail.com>
2017-01-18 16:36:11 +0000

    commit

--- RedBlackTrees.hs
+++ RedBlackTrees.hs
@@ -3,4 +3,4 @@ data RBT a = E | N a Color (RBT a) (RBT a) deriving (Show)
 
 toBlack :: (RBT a) -> (RBT a)
 toBlack E = error"Empty Node"
-toBlack (N x c l r) = (N x BLACK l r)
+toBlack (N x c l r)  = (N x BLACK l r)
